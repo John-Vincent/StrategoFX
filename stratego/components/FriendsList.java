783ec616b3dd44c9b61c82b3685686e00041d18e
Bradley <bdrhein@iastate.edu>
2017-02-21 15:28:36 -0600

    Finished Friend components.

--- stratego/components/FriendsList.java
+++ stratego/components/FriendsList.java
@@ -1,10 +1,10 @@
 package stratego.components;
 
-import java.util.ArrayList;
+import java.util.Observable;
 import java.util.Observer;
-
 import javafx.application.Platform;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import stratego.components.FriendModel;
 
@@ -12,14 +12,17 @@ public class FriendsList extends VBox implements Observer {
 	public static final int smallPadding = 3;
 
 	@Override
-	public void update(Observable arg0, Object arg1) {
-		FriendModel m = (FriendModel) arg0;
-		FriendVBox[] friendBoxes = createBoxes(friends);
+	public void update(Observable friends, Object arg1) {
+		FriendModel fm = (FriendModel) friends;
+		FriendVBox[] friendBoxes = createBoxes(fm, fm.getFriendListSize());
 
 		Platform.runLater(new Runnable() {
 			@Override
 			public void run() {
-				// TODO
+				getChildren().clear();
+				for (FriendVBox box : friendBoxes) {
+					getChildren().add(box.getVBox());
+				}
 			}
 
 		});
@@ -27,18 +30,31 @@ public class FriendsList extends VBox implements Observer {
 	}
 
 	public FriendVBox[] createBoxes(FriendModel friends, int size) {
-		FriendVBox[] friendBoxes = new Label[size];
+		FriendVBox[] friendBoxes = new FriendVBox[size];
 
 		for (int i = 0; i < size; i++) {
-			 f = new FriendVBox(friends.getFriend(i).getName(), freinds.getFriend(i).getStatus()); //friend name and status)
+			friendBoxes[i] = new FriendVBox(friends.getFriendName(i), friends.getFriendStatus(i));
 		}
+		return friendBoxes;
 	}
 
 	private class FriendVBox {
 		private VBox box;
+		private Label name;
+		private Label status;
 
 		private FriendVBox(String name, String status) {
-			box = new VBox(smallPadding, name, status);
+			this.name = new Label(name);
+			this.status = new Label(status);
+			box = new VBox(smallPadding, this.name, this.status);
+			VBox.setVgrow(this.name, Priority.ALWAYS);
+			VBox.setVgrow(this.status, Priority.ALWAYS);
+			this.name.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			this.status.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		}
+
+		private VBox getVBox() {
+			return box;
 		}
 	}
 
