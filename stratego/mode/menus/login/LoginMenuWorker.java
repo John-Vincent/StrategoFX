54d5fe4e4b8902ccefa4ae76141d14184e772347
Bradley <bdrhein@iastate.edu>
2017-02-21 23:00:21 -0600

    Fixing bugs in worker.

--- stratego/mode/menus/login/LoginMenuWorker.java
+++ stratego/mode/menus/login/LoginMenuWorker.java
@@ -5,48 +5,56 @@ import stratego.network.Networker;
 import java.net.*;
 import stratego.mode.ModeWorker;
 
-public class LoginMenuWorker extends ModeWorker{
+public class LoginMenuWorker extends ModeWorker {
 
-  public LoginMenuWorker(ConcurrentLinkedQueue<Runnable> q){
-    super(q);
-  }
+	public LoginMenuWorker(ConcurrentLinkedQueue<Runnable> q) {
+		super(q);
+	}
 
-  @Override
-  public Runnable getRequest(String name, Object... args){
-    switch(name){
-      case "signin":
-        return new SignInRequest((String) args[0], (String) args[1]);
-      case "signup":
-    	  return new SignUpRequest();
-      default:
-        return null;
-    }
-  }
+	@Override
+	public Runnable getRequest(String name) {
+		switch (name) {
+		default:
+			return null;
+		}
+	}
 
-  private class SignInRequest implements Runnable{
-    String username;
-    String password;
+	
+	@Override
+	public Runnable getRequest(String name, Object... args) {
+		switch (name) {
+		case "signin":
+			return new SignInRequest((String) args[0], (String) args[1]);
+		case "signup":
+			return new SignUpRequest();
+		default:
+			return null;
+		}
+	}
 
-    public SignInRequest(String name, String pass){
-      username=name;
-      password=pass;
-    }
-    @Override
-    public void run(username,password){
-      net.sendSignInRequest(username,password);
-      //this terminates the execution of this worker advancing the program to the next UI
-      setRunning(false);
-    }
-  }
+	private class SignInRequest implements Runnable {
+		String username;
+		String password;
 
-    private class SignUpRequest implements Runnable{
-        @Override
-        public void run(){
-            setRunning(false);
-        }
-    }
+		public SignInRequest(String name, String pass) {
+			username = name;
+			password = pass;
+		}
 
+		@Override
+		public void run() {
+			net.login(username, password);
+			// this terminates the execution of this worker advancing the
+			// program to the next UI
+			setRunning(false);
+		}
+	}
 
+	private class SignUpRequest implements Runnable {
+		@Override
+		public void run() {
+			setRunning(false);
+		}
+	}
 
-  }
 }
