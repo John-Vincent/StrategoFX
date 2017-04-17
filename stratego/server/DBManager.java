316d16b9486015cbde3c84a87c4979383cc4c9ef
ryanm <ryanmccullough21@gmail.com>
2017-04-16 21:32:07 -0500

    added queries for server table

--- stratego/server/DBManager.java
+++ stratego/server/DBManager.java
@@ -24,7 +24,7 @@ public final class DBManager{
                                             "where f.senderid = (select u2.id from `user` u2 where u2.name =?);";
 
 											//changed friend requests to fit new table
-  private static final String requestFriendQ = "insert into friendrequests(senderid,receiverid) "+
+  private static final String requestFriendQ = "insert into 'friendrequests'('senderid','receiverid') "+
                                   "values((select u.id from `user` u where u.name= ? ),(select u2.id from `user` u2 where u2.name = ? ));";
 
 								  //accepting friend requests requires a pair of insertions to friends and a deletion from friendrequests
@@ -41,13 +41,13 @@ public final class DBManager{
   private static final String activateServer = "";
 
   //query to add a server into the database with a given server name, password(32 bytes), and sessionID(int)
-  private static final String makeServer = "";
+  private static final String makeServer = "insert into 'server'('name','password','sessionid') values ( ? , ?, ?);";
 
   //query to return the server sessionID(int)
-  private static final String findServer = "";
+  private static final String findServer = "select s.sessionid from 'server' s where s.name = ?;";
 
   //query to set the "active" value of the server to 0/false
-  private static final String deactivateServer;
+  private static final String deactivateServer = "update 'server' s set 'active' = '0' where s.name = ?;";
 
   static{
     source.setDriverClassName("com.mysql.jdbc.Driver");
